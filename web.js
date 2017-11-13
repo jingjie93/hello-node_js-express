ebdd664c0ce56369360fb400fff68ea800022b0e
jingjie93 <33574066+jingjie93@users.noreply.github.com>
2017-11-13 21:05:53 +0800

    Update web.js

--- web.js
+++ web.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('SCM Lab Assignment\n Name:Lim Jing Jie');
+  res.send('SCM Lab Assignment\n <br> Lim Jing Jie CB15170 <br> Wong Ying Shin CB15172 <br> Kho Kheng Eian CB15173 <br> Zahra Syahida CB15169 <br> ');
 });
 
 app.listen(process.env.PORT || 3000);
