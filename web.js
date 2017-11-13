ffbee80dacd603f2907043eed4762272fb3eca47
jingjie93 <33574066+jingjie93@users.noreply.github.com>
2017-11-13 21:22:42 +0800

    Update web.js

--- web.js
+++ web.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('hello, world');
+  res.send('SCM Lab Assignment 1 & 2 <br> Group 6 <br> Lim Jing Jie CB15170');
 });
 
 app.listen(process.env.PORT || 3000);
